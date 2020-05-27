0c4de0358597747f79f38c26adcebf0e4fda1547
chenhao <chenhao-3@thunisoft.com>
2020-05-27 18:44:52 +0800

    111

---
+++ caffeine-monitor-demo/src/main/java/cc/CaffineMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.github.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffineMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffineMonitorApplication.class, args);
+	}
+
+}
